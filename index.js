9a9dc61af46b0933f2ea2e8c32cdce331a2ba071
dsantacruz <diegomiguel114@gmail.com>
2016-10-13 17:32:28 -0500

    api rest with param

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict'
-
+//Nodemon escucha cualquier cambio y lo refleja en el navegador (como livereload)
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -8,6 +8,10 @@ const port = process.env.PORT || 3001
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.get('/hola/:name', (req, res) => {
+	res.send({ message: `Hola ${req.params.name}!` })
+})
 
 app.listen(port, () => {
 	console.log(`API REST corriendo en http://localhost:${port}`)
